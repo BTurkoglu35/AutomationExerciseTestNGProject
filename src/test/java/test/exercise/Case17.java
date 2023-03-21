eafe7dee518ea6465db643aece8ae9334ed226db
Busra Turkoglu <bturkoglu9393@gmail.com>
2023-03-21 21:38:52 +0300

    21.03

--- src/test/java/test/exercise/Case17.java
+++ src/test/java/test/exercise/Case17.java
@@ -6,35 +6,44 @@ import pages.ProducCartAutomationExercisePage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
-public class Case17 {
+public class Case17 extends TestBaseRapor {
     ProducCartAutomationExercisePage autoE = new ProducCartAutomationExercisePage();
     SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void testName() {
-        //     1. Tarayıcıyı başlatın
-        //     2. 'http://automationexercise.com' url'sine gidin
+        extentTest = extentReports.createTest(" Cart", "Verify that the product has been removed from the cart");
+
         Driver.getDriver().get(ConfigReader.getProperty("automationexerciseUrl"));
-        //     3. Ana sayfanın başarıyla görünür olduğunu doğrulayın
+        extentTest.info("goed to url");
+
         softAssert.assertTrue(autoE.anasayfa.isDisplayed(), "anasayfa goruntulenmedi");
         String expectedUrl = "https://www.automationexercise.com/";
         String actualUrl = Driver.getDriver().getCurrentUrl();
         softAssert.assertEquals(expectedUrl, actualUrl, "anasayfa goruntulenmedi");
-        //     4. Sepete ürün ekleyin
+
+
         ReusableMethods.jsScroll(autoE.ilkUrunAddCart);
         autoE.ilkUrunAddCart.click();
-        //     5. 'Sepet' düğmesini tıklayın
+        extentTest.info("Item added to cart");
+
+
         autoE.viewCartButton.click();
-        //     6. Sepet sayfasının görüntülendiğini doğrulayın
+        extentTest.info("Clicked cart button");
+
         softAssert.assertTrue(autoE.shoppingCartText.isDisplayed());
-        //     7. Belirli bir ürüne karşılık gelen 'X' düğmesini tıklayın
+        extentTest.info("Cart page is displayed");
+
         int ilkSize = autoE.cartDeleteButton.size();
         autoE.cartDeleteButton.get(0).click();
-        //     8. Ürünün sepetten çıkarıldığını doğrulayın
+        extentTest.info("Clicked delete button");
+
         ReusableMethods.waitFor(5);
         int sonSize = autoE.cartDeleteButton.size();
         softAssert.assertTrue((ilkSize - 1) == sonSize);
+        extentTest.info("Verified that the product has been removed from the cart");
         softAssert.assertAll();
     }
 }
